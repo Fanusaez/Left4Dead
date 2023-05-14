699f8f2fd7942d9a4143d40bba77730e1cfa8707
ibertolini <56204692+nacho-1@users.noreply.github.com>
2023-05-14 15:30:35 -0300

    Graphics testing.

---
+++ src/client_main.cpp
@@ -0,0 +1,89 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2pp/SDL2pp.hh>
+#include "Player.h"
+#include <iostream>
+#include <exception>
+#include <unistd.h>
+
+
+static bool handleEvents(Player &player);
+static void render(SDL2pp::Renderer &renderer, Player &player);
+static void update(Player &player, float dt);
+
+int main(int argc, char *argv[])
+{
+	try {
+		SDL2pp::SDL sdl(SDL_INIT_VIDEO);
+		SDL2pp::SDLImage image(IMG_INIT_PNG);
+		SDL2pp::Window window("Hello world", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+				      800, 600,
+				      SDL_WINDOW_RESIZABLE);
+		SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+		SDL2pp::Texture im(renderer,
+				   SDL2pp::Surface("../resources/Soldier_1/Walk.png").SetColorKey(true, 0));
+		Player player(im);
+		bool running = true;
+
+		while (running) {
+			running = handleEvents(player);
+			update(player, FRAME_RATE);
+			render(renderer, player);
+			// la cantidad de segundos que debo dormir se debe ajustar en función
+			// de la cantidad de tiempo que demoró el handleEvents y el render
+			usleep(FRAME_RATE);
+		}
+	} catch (std::exception &e) {
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
+
+static bool handleEvents(Player &player) {
+	SDL_Event event;
+	// Para el alumno: Buscar diferencia entre waitEvent y pollEvent
+	while(SDL_PollEvent(&event)){
+		switch(event.type) {
+			case SDL_KEYDOWN: {
+				// ¿Qué pasa si mantengo presionada la tecla?
+				SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) event;
+				switch (keyEvent.keysym.sym) {
+					case SDLK_LEFT:
+						player.moveLeft();
+						break;
+					case SDLK_RIGHT:
+						player.moveRigth();
+						break;
+				}
+			} // Fin KEY_DOWN
+				break;
+			case SDL_KEYUP: {
+				SDL_KeyboardEvent& keyEvent = (SDL_KeyboardEvent&) event;
+				switch (keyEvent.keysym.sym) {
+					case SDLK_LEFT:
+						player.stopMoving();
+						break;
+					case SDLK_RIGHT:
+						player.stopMoving();
+						break;
+				}
+			}// Fin KEY_UP
+				break;
+			case SDL_QUIT:
+				std::cout << "Quit :(" << std::endl;
+				return false;
+		} // fin switch(event)
+	} // fin while(SDL_PollEvents)
+	return true;
+}
+
+static void render(SDL2pp::Renderer &renderer, Player &player) {
+	renderer.Clear();
+	player.render(renderer);
+	renderer.Present();
+}
+
+static void update(Player &player, float dt) {
+	player.update(dt);
+}
