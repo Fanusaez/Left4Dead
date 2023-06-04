938b72ca813ac764e76b1e1b6ea1209bb62263c7
ibertolini <56204692+nacho-1@users.noreply.github.com>
2023-06-04 19:02:09 -0300

    bugfix: Corrigo nombre erroneo de archivo
    Era Shot_1 y no Shoot_1

--- src/graphics/gameview_configs_loader.cpp
+++ src/graphics/gameview_configs_loader.cpp
@@ -13,7 +13,7 @@ GameviewConfigurationsLoader::GameviewConfigurationsLoader() :
 {
 	this->spritesToLoad.push_back("Soldier_1/Walk.png");
 	this->spritesToLoad.push_back("Soldier_1/Idle.png");
-	this->spritesToLoad.push_back("Soldier_1/Shoot_1.png");
+	this->spritesToLoad.push_back("Soldier_1/Shot_1.png");
 	this->spritesToLoad.push_back("Soldier_1/Recharge.png");
 	this->spritesToLoad.push_back("backgrounds/War1/Pale/Full_Sky.png");
 	this->spritesToLoad.push_back("backgrounds/War1/Pale/Far_Background.png");
