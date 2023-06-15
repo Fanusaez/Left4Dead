891de374d3a9748efc362bda9e006a4e986f6f23
fanusaez <you@example.com>
2023-06-15 11:12:24 -0300

    fix: zombie mas rapido no queda atrapado atras de los lentos.

--- src/game_logic/chaser.cpp
+++ src/game_logic/chaser.cpp
@@ -15,72 +15,102 @@ Chaser::Chaser(Zombie *zombie, GameMap& map, std::int16_t& x_pos, std::int16_t& 
                 map(map) {}
 
 void Chaser::chase(std::int16_t x_pos_chase, std::int16_t y_pos_chase) {
+    bool same_place = true;
 
-    //std::int16_t x_matrix_sold = closest_soldier->get_x_matrix_pos();
-    //std::int16_t y_matrix_sold = closest_soldier->get_y_matrix_pos();
+    if (y_pos_chase > y_pos && same_place) { // movimiento para abajo
+        move_down(same_place);
+    } else if (y_pos_chase < y_pos && same_place) { // movimiento para arriba
+        move_up(same_place);
+    }
+
+    if (x_pos_chase < x_pos && same_place) { // movimiento para izquierda
+        move_left(same_place);
+    } else if (x_pos_chase > x_pos && same_place) { // movimiento para derecha
+        move_right(same_place);
+    }
+
+    if (same_place) { // para escapar de pos bloqueada
+        if (y_pos % MOVEMENTS_PER_CELL == 0) { // mov para arriba
+            move_up(same_place);
+        } else {
+            move_down(same_place);
+        }
+        if (x_pos_chase < x_pos) { // movimiento para izquierda
+            move_left(same_place);
+        } else { // movimiento para derecha
+            move_right(same_place);
+        }
+    }
+}
+
+void Chaser::move_down(bool &same_place) {
     std::int16_t x_matrix_walker = zombie->get_x_matrix_pos();
     std::int16_t y_matrix_walker = zombie->get_y_matrix_pos();
-
-    bool same_place = true;
-    /*
-     * Si utilizo x_matrix_sold y x_matrix_walker va a parar de moverse una vez que coincidan
-     * las columnas (aunque los bichos no esten totalmente alineados visualmente)
-     * Si utilizo las posiciones en pixeles (sin hacer la division " ... / MOV_PER_CEL ")
-     * Se van a alinear totalmente. Esto puede traer problemas al querer estar ajustando
-     * el movimiento y pasar a atacar despues (creo). Por ahora pongo que se align totalmente
-     */
-    std::int16_t x_new_pos = INVALID_POSITION;
     std::int16_t y_new_pos = INVALID_POSITION;
-    if (y_pos_chase > y_pos && same_place) { // movimiento para abajo
-        std::int16_t y_new = y_pos + zombie_speed;
-        if ((y_new % MOVEMENTS_PER_CELL) != 0) {
-            y_pos += zombie_speed;
-            return;
-        }
-        map.move_object_down(x_matrix_walker, y_matrix_walker, y_new_pos);
-        if (y_new_pos != INVALID_POSITION) {
-            y_pos += zombie_speed;
-            same_place = false;
-        }
-    } else if (y_pos_chase < y_pos && same_place) { // movimiento para arriba
-        // if (y_pos <= WALLS_LIMITS) return nullptr;
-        if ((y_pos % MOVEMENTS_PER_CELL) != 0) {
-            y_pos -= zombie_speed;
-            return;
-        }
-        map.move_object_up(x_matrix_walker, y_matrix_walker, y_new_pos);
-        if (y_new_pos != INVALID_POSITION) {
-            same_place = false;
-            y_pos -= zombie_speed;
-        }
-    }
-    if (x_pos_chase < x_pos && same_place) { // movimiento para izquierda
-        zombie->set_direction(LEFT);
-        //if (x_pos <= WALLS_LIMITS) return nullptr; por ahora no hay walls limit
-        if ((x_pos % MOVEMENTS_PER_CELL) != 0) {
-            x_pos -= zombie_speed;
-            return;
-        }
-        map.move_object_left(x_matrix_walker, y_matrix_walker, x_new_pos);
-        if (x_new_pos != INVALID_POSITION) {
-            same_place = false;
-            x_pos -= zombie_speed;
-        }
-    } else if (x_pos_chase > x_pos && same_place) { // movimiento para derecha
-        zombie->set_direction(RIGHT);
-        std::int16_t x_new = x_pos + zombie_speed;
-        if ((x_new % MOVEMENTS_PER_CELL) != 0) {
-            x_pos += zombie_speed;
-            return;
-        }
-        map.move_object_right(x_matrix_walker, y_matrix_walker, x_new_pos);
-        if (x_new_pos != INVALID_POSITION) {
-            same_place = false;
-            x_pos += zombie_speed;
-        }
-    }
 
-    if (same_place) { // se quedo trababado por obstaculo, decido que el zombie puede atacar de costado
-        //move_soldier_left(floor(x_pos), floor(y_pos), x_new_pos);
+    std::int16_t y_new = y_pos + zombie_speed;
+    if ((y_new % MOVEMENTS_PER_CELL) != 0) {
+        y_pos += zombie_speed;
+        same_place = false;
+        return;
+    }
+    map.move_object_down(x_matrix_walker, y_matrix_walker, y_new_pos);
+    if (y_new_pos != INVALID_POSITION) {
+        y_pos += zombie_speed;
+        same_place = false;
+    }
+}
+
+void Chaser::move_up(bool &same_place) {
+    std::int16_t x_matrix_walker = zombie->get_x_matrix_pos();
+    std::int16_t y_matrix_walker = zombie->get_y_matrix_pos();
+    std::int16_t y_new_pos = INVALID_POSITION;
+
+    if ((y_pos % MOVEMENTS_PER_CELL) != 0) {
+        y_pos -= zombie_speed;
+        same_place = false;
+        return;
+    }
+    map.move_object_up(x_matrix_walker, y_matrix_walker, y_new_pos);
+    if (y_new_pos != INVALID_POSITION) {
+        same_place = false;
+        y_pos -= zombie_speed;
+    }
+}
+
+void Chaser::move_right(bool &same_place) {
+    std::int16_t x_matrix_walker = zombie->get_x_matrix_pos();
+    std::int16_t y_matrix_walker = zombie->get_y_matrix_pos();
+    std::int16_t x_new_pos = INVALID_POSITION;
+
+    zombie->set_direction(RIGHT);
+    std::int16_t x_new = x_pos + zombie_speed;
+    if ((x_new % MOVEMENTS_PER_CELL) != 0) {
+        x_pos += zombie_speed;
+        same_place = false;
+        return;
+    }
+    map.move_object_right(x_matrix_walker, y_matrix_walker, x_new_pos);
+    if (x_new_pos != INVALID_POSITION) {
+        same_place = false;
+        x_pos += zombie_speed;
+    }
+}
+
+void Chaser::move_left(bool &same_place) {
+    std::int16_t x_matrix_walker = zombie->get_x_matrix_pos();
+    std::int16_t y_matrix_walker = zombie->get_y_matrix_pos();
+    std::int16_t x_new_pos = INVALID_POSITION;
+
+    zombie->set_direction(LEFT);
+    if ((x_pos % MOVEMENTS_PER_CELL) != 0) {
+        x_pos -= zombie_speed;
+        same_place = false;
+        return;
+    }
+    map.move_object_left(x_matrix_walker, y_matrix_walker, x_new_pos);
+    if (x_new_pos != INVALID_POSITION) {
+        same_place = false;
+        x_pos -= zombie_speed;
     }
 }
--- src/game_logic/chaser.h
+++ src/game_logic/chaser.h
@@ -14,6 +14,11 @@ private:
     GameMap& map;
     const std::int16_t zombie_speed = 1;
 
+    void move_right(bool& same_place);
+    void move_left(bool& same_place);
+    void move_up(bool& same_place);
+    void move_down(bool& same_place);
+
 public:
 
     Chaser(Zombie* zombie, GameMap& map, std::int16_t& x_pos, std::int16_t& y_pos);
