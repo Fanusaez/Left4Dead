07b5736de79dcff8350003abe18035d421cff698
fanusaez <you@example.com>
2023-06-22 09:21:31 -0300

    fix: Zombies cambian de direccion hacia la tuya ni bien te detectan.

--- src/game_logic/chaser.cpp
+++ src/game_logic/chaser.cpp
@@ -16,6 +16,7 @@ Chaser::Chaser(Zombie *zombie, GameMap& map, std::int16_t& x_pos, std::int16_t& 
 
 void Chaser::chase(std::int16_t x_pos_chase, std::int16_t y_pos_chase) {
     bool same_place = true;
+    change_direction_towards_soldier(x_pos_chase, y_pos_chase);
     if (y_pos_chase > y_pos && same_place) { // movimiento para abajo
         move_down(same_place);
     } else if (y_pos_chase < y_pos && same_place) { // movimiento para arriba
@@ -43,6 +44,14 @@ void Chaser::chase(std::int16_t x_pos_chase, std::int16_t y_pos_chase) {
         } else {
             move_right(same_place);
         }
+    }
+}
+
+void Chaser::change_direction_towards_soldier(std::int16_t x_pos_chase, std::int16_t y_pos_chase) {
+    if (x_pos_chase < x_pos) {
+        zombie->set_direction(LEFT);
+    } else {
+        zombie->set_direction(RIGHT);
     }
 }
 
@@ -86,7 +95,6 @@ void Chaser::move_right(bool &same_place) {
     std::int16_t y_matrix_walker = zombie->get_y_matrix_pos();
     std::int16_t x_new_pos = INVALID_POSITION;
 
-    zombie->set_direction(RIGHT);
     std::int16_t x_new = x_pos + zombie_speed;
     if ((x_new % movements_per_cell) != 0) {
         x_pos += zombie_speed;
@@ -105,7 +113,6 @@ void Chaser::move_left(bool &same_place) {
     std::int16_t y_matrix_walker = zombie->get_y_matrix_pos();
     std::int16_t x_new_pos = INVALID_POSITION;
 
-    zombie->set_direction(LEFT);
     if ((x_pos % movements_per_cell) != 0) {
         x_pos -= zombie_speed;
         same_place = false;
